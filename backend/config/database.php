901e00cd0f5dad0267808b9dd302faf2fce9ddaa
schlibra <schlibra@163.com>
2024-06-23 22:51:51 +0800

    更新数据库配置

--- backend/config/database.php
+++ backend/config/database.php
@@ -22,23 +22,23 @@ return [
     'connections'     => [
         'mysql' => [
             // 数据库类型
-            'type'            => env('DB_TYPE', 'mysql'),
+            'type'            => 'mysql',
             // 服务器地址
-            'hostname'        => env('DB_HOST', '127.0.0.1'),
+            'hostname'        => '127.0.0.1',
             // 数据库名
-            'database'        => env('DB_NAME', ''),
+            'database'        => 'meme',
             // 用户名
-            'username'        => env('DB_USER', 'root'),
+            'username'        => 'meme',
             // 密码
-            'password'        => env('DB_PASS', ''),
+            'password'        => '123456',
             // 端口
-            'hostport'        => env('DB_PORT', '3306'),
+            'hostport'        => '3306',
             // 数据库连接参数
             'params'          => [],
             // 数据库编码默认采用utf8
-            'charset'         => env('DB_CHARSET', 'utf8'),
+            'charset'         => 'utf8',
             // 数据库表前缀
-            'prefix'          => env('DB_PREFIX', ''),
+            'prefix'          => '',
 
             // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
             'deploy'          => 0,
