5ab71b45553af500c472140fb389fc07b2a6d531
schlibra <schlibra@163.com>
2024-06-23 19:17:53 +0800

    更新main.js，引入相关的模块，移除无用样式

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,23 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import vuetyped from 'vue3typed'
+import "element-plus/dist/index.css"
+import "element-plus/theme-chalk/dark/css-vars.css"
+import zhCn from "element-plus/es/locale/lang/zh-cn"
 import App from './App.vue'
 import router from './router'
+import { useDark } from "@vueuse/core"
 
 const app = createApp(App)
+const isDark = useDark()
+if (isDark.value) {
+    document.body.classList.add("dark")
+}
 
 app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.use(vuetyped)
 
 app.mount('#app')
