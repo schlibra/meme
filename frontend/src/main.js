d6e564ae5ec462ae1f184301107ea911612531f3
yaoyangyaha <yaoyangyaha@163.com>
2024-06-23 17:38:34 +0800

    add new import：element plus

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
