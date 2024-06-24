82a5f7e6cd0d90d50efc7cbd6ec6512a8ae73751
schlibra <schlibra@163.com>
2024-06-24 16:41:21 +0800

    增加一个控制器，用于加载静态资源

---
+++ backend/app/controller/Assets.php
@@ -0,0 +1,37 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\controller;
+
+use think\Request;
+
+class Assets
+{
+    public function index(Request$request)
+    {
+        $filename = $request->url();
+//        file_exists()
+        $filepath = root_path() . "view/dist/$filename";
+        if (file_exists($filepath)) {
+            $ext = explode(".", $filename);
+            $ext = end($ext);
+            $type = match ($ext) {
+                "js" => "application/javascript",
+                "css" => "text/css",
+                default => "text/plain",
+            };
+            header("Content-Type: $type");
+            $data = file_get_contents($filepath);
+            return response($data)->header([
+                "Content-Type" => $type
+            ]);
+        } else {
+            return json([
+                "code" => 404,
+                "msg" => "文件不存在",
+                "filename" => $filename,
+                "realPath" => $filepath
+            ])->code(404);
+        }
+    }
+}
