c9886e1204480da14c13bf636cee0d4723e19d8b
schlibra <schlibra@163.com>
2024-06-24 16:39:22 +0800

    支持后端加载html

--- backend/app/controller/Index.php
+++ backend/app/controller/Index.php
@@ -3,8 +3,12 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\facade\View;
 
 class Index extends BaseController
 {
-
+    function index(): string
+    {
+        return View::fetch("dist/index");
+    }
 }
