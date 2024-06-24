2f05c0e5d9d2cb907921fcfe553d215b0aff6ff0
yaoyangyaha <yaoyangyaha@163.com>
2024-06-24 20:35:14 +0800

    change the homeview‘s image viewer

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -13,6 +13,17 @@ const currentPage = ref(1)
 const pageSize = ref(20)
 const value = ref(5.0)
 const isShow = ref(false)
+const url =
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg'
+const srcList = [
+  'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+  'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
+  'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
+  'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
+  'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
+  'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
+]
 
 onMounted(()=>{
   if (token) {
@@ -99,7 +110,17 @@ function logout() {
       <el-col :span="6" v-for="i in 20">
         <el-card class="img-card">
           <template #default>
-            <el-image src="https://liukaili.netlify.app/favicon1.ico"></el-image>
+            <el-image
+              style="width: 100px; height: 100px"
+              :src="url"
+              :zoom-rate="1.2"
+              :max-scale="7"
+              :min-scale="0.2"
+              :preview-src-list="srcList"
+              :initial-index="4"
+              fit="cover"
+            />
+              
           </template>
           <template #footer>
             <el-text :type="'primary'">梗图描述</el-text>
@@ -158,5 +179,15 @@ function logout() {
 }
 .img-card {
   margin-bottom: 8px;
+}
+.demo-image__error .image-slot {
+  font-size: 30px;
+}
+.demo-image__error .image-slot .el-icon {
+  font-size: 30px;
+}
+.demo-image__error .el-image {
+  width: 100%;
+  height: 200px;
 }
 </style>
