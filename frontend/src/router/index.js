2f6419faa840be5acaf2faafd54fe5bd49f29c55
schlibra <schlibra@163.com>
2024-06-25 00:15:12 +0800

    更新路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from '@/views/RegisterView.vue';
 import ForgetView from '@/views/ForgetView.vue';
