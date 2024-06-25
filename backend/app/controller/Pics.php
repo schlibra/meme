32dfb603d852adaee3be618c4efaa494170b65c5
schlibra <schlibra@163.com>
2024-06-25 15:28:53 +0800

    完成图片上传，图片列表，图片预览接口

--- backend/app/controller/Pics.php
+++ backend/app/controller/Pics.php
@@ -3,86 +3,155 @@ declare (strict_types = 1);
 
 namespace app\controller;
 
+use Firebase\JWT\BeforeValidException;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
+use think\facade\Db;
 use think\Request;
+use function Symfony\Component\VarDumper\Dumper\esc;
 
 class Pics
 {
-    /**
-     * 显示资源列表
-     *
-     * @return \think\Response
-     */
-    public function index()
+    public function index(Request$request)
     {
-        //
-        return "index";
+        $token = $request->header("Authorization", "");
+        $showDelete = $request->get("showDelete", "");
+        $userId = null;
+        if (str_starts_with($token, "Bearer")) {
+            $token = str_replace("Bearer ", "", $token);
+            try {
+                $data = (array)JWT::decode($token, new Key("meme_login_token_key", "HS256"));
+                $_username = $data["username"];
+                $_email = $data["email"];
+                $user = Db::connect("mysql")
+                    ->table("user")
+                    ->where("username", $_username)
+                    ->where("email", $_email)
+                    ->find();
+                if ($user) {
+                    $userId = $user["id"];
+                }
+            } catch (SignatureInvalidException|\DomainException|BeforeValidException|ExpiredException$e) {}
+        }
+        $allUser = Db::connect("mysql")
+            ->table("user")
+            ->select();
+        $userList = [];
+        foreach ($allUser as $user) {
+            $userList[$user["id"]] = $user;
+        }
+        if ($showDelete === "Y") {
+            $pics = Db::connect("mysql")
+                ->table("pics")
+                ->select();
+        } else {
+            $pics = Db::connect("mysql")
+                ->table("pics")
+                ->where("delete", "=", null)
+                ->select();
+        }
+        for ($i = 0; $i < count($pics); ++$i) {
+            $pic = $pics[$i];
+            $userId = $pic["user"];
+            if (isset($userList[$userId])) {
+                $pic["nickname"] = $userList[$userId]["nickname"];
+            } else {
+                $pic["nickname"] = "未知用户";
+            }
+            unset($pic["data"]);
+            unset($pic["type"]);
+            $pic["url"] = $request->domain() . "/pics/" . $pic["id"];
+            $pics[$i] = $pic;
+        }
+        return json(["code" => 200, "msg" => "数据获取成功", "data" => $pics]);
     }
 
-    /**
-     * 显示创建资源表单页.
-     *
-     * @return \think\Response
-     */
     public function create(Request$request)
     {
-
-        return "create";
+        $image = chunk_split(base64_encode(file_get_contents($request->file("image")->getPathname())));
+        $type = $request->file("image")->getMime();
+        if (empty($image)) {
+            return json(["code" => 400, "msg" => "未选择图片"]);
+        }
+        $token = $request->header("Authorization", "");
+        $description = $request->post("description", "");
+        $name = $request->post("name", "");
+        if (str_starts_with($token, "Bearer")) {
+            $token = str_replace("Bearer ", "", $token);
+            try {
+                $data = (array) JWT::decode($token, new Key("meme_login_token_key", "HS256"));
+                $_username = $data["username"];
+                $_email = $data["email"];
+                $user = Db::connect("mysql")
+                    ->table("user")
+                    ->where("username", $_username)
+                    ->where("email", $_email)
+                    ->find();
+                if ($user) {
+                    $permission = Db::connect("mysql")
+                        ->table("group")
+                        ->where("id", $user["group"])
+                        ->find();
+                    if ($permission) {
+                        if ($permission["upload"] === "Y") {
+                            Db::connect("mysql")
+                                ->table("pics")
+                                ->insert([
+                                    "name" => $name,
+                                    "description" => $description,
+                                    "user" => $user["id"],
+                                    "data" => $image,
+                                    "type" => $type,
+                                    "create" => date("Y-m-d H:i:s"),
+                                    "update" => date("Y-m-d H:i:s")
+                                ]);
+                            return json(["code" => 200, "msg" => "上传成功"]);
+                        } else {
+                            return json(["code" => 403, "msg" => "没有上传权限"]);
+                        }
+                    } else {
+                        return json(["code" => 401, "msg" => "没有权限"]);
+                    }
+                } else {
+                    return json(["code" => 401, "msg" => "用户不存在"]);
+                }
+            } catch (SignatureInvalidException|\DomainException|BeforeValidException|ExpiredException$e) {
+                return json(["code"=>401, "msg"=>"Token信息错误：" . $e->getMessage()]);
+            }
+        } else {
+            return json(["code" => 403, "msg" => "未登录账号"]);
+        }
     }
 
-    /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
-     */
     public function save(Request $request)
     {
         //
     }
-
-    /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
     public function read($id)
     {
-        //
-        return "get by id $id";
+        $data = Db::connect("mysql")
+            ->table("pics")
+            ->where("id", $id)
+            ->find();
+        if ($data) {
+            $img = base64_decode($data["data"]);
+            $type = $data["type"];
+            return response($img)->header([
+                "Content-Type" => $type
+            ]);
+        } else {
+            return json(["code" => 404, "msg" => "图片不存在"]);
+        }
     }
-
-    /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
-
-    /**
-     * 删除指定资源
-     *
-     * @param  int  $id
-     * @return \think\Response
-     */
     public function delete($id)
     {
         //
+        return "delete";
     }
 }
