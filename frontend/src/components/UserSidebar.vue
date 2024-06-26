4835e14344c02956a3dca53f0684bf9152d75ffd
schlibra <schlibra@163.com>
2024-06-26 23:43:52 +0800

    用户页面顶栏侧栏组件

---
+++ frontend/src/components/UserSidebar.vue
@@ -0,0 +1,34 @@
+<script setup>
+const prop = defineProps(["defaultActive"])
+const defaultActive = prop.defaultActive
+</script>
+
+<template>
+  <el-menu :default-active="defaultActive" class="menu">
+    <el-menu-item disabled>
+      <h3>用户中心</h3>
+    </el-menu-item>
+    <el-menu-item index="1">
+      <el-icon><Menu /></el-icon>
+      <span>基本设置</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><Lock /></el-icon>
+      <span>用户权限</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><Picture /></el-icon>
+      <span>图片列表</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><ChatLineRound /></el-icon>
+      <span>评论列表</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped lang="scss">
+.menu {
+  height: 100vh;
+}
+</style>
---
+++ frontend/src/components/UserTop.vue
@@ -0,0 +1,22 @@
+<script setup>
+import router from "@/router/index.js";
+
+const prop = defineProps(["title"])
+const title = "用户中心 - " + prop.title
+
+function gotoHome() {
+  router.push({
+    path: "/"
+  })
+}
+</script>
+
+<template>
+  <el-page-header class="header" title="返回首页" @back="gotoHome" :content="title" />
+</template>
+
+<style scoped lang="scss">
+.header {
+  margin-top: 8px;
+}
+</style>
