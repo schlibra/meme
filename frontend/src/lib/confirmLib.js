2e1645dea52486f423f1e16acb59582598eb0f0f
schlibra <schlibra@163.com>
2024-07-09 10:58:59 +0800

    优化确认框逻辑

--- frontend/src/lib/confirmLib.js
+++ frontend/src/lib/confirmLib.js
@@ -4,13 +4,13 @@ function confirm(message, title, action={confirm: _=>_, cancel: _=>_, close: _=>
     ElMessageBox.confirm(message, title, {
         type: "info"
     }).then(_action => {
-        if (_action === "confirm") {
-            action.confirm()
-        } else if (_action === "cancel") {
-            action.cancel()
-        } else if (_action === "close") {
-            action.close()
-        }
+        console.log(_action)
+        if (_action === "confirm" && action.confirm) action.confirm()
+        if (_action === "cancel" && action.cancel) action.cancel()
+        if (_action === "close" && action.close) action.close()
+    }).catch(_action => {
+        if (_action === "cancel" && action.cancel) action.cancel()
+        if (_action === "close" && action.close) action.close()
     })
 }
 
