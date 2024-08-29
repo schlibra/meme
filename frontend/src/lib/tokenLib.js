a35772bdd73f90da1b0c806893c49850c1d5ce35
schlibra <schlibra@163.com>
2024-08-29 21:01:41 +0800

    写入token时同时保存至cookie

--- frontend/src/lib/tokenLib.js
+++ frontend/src/lib/tokenLib.js
@@ -1,6 +1,9 @@
+import { useCookies } from "vue3-cookies";
+const { cookies } = useCookies()
 function setToken(token) {
     let _token = typeof token === "string" ? token : token.data.token
     localStorage.setItem("token", _token)
+    cookies.set("token", _token)
 }
 function getToken() {
     return localStorage.getItem("token")
